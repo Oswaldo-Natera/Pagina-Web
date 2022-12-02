108012dd20930551c14a62b61e1160ea18eeefde
Oswaldo Natera <oswaldonateram@gmail.com>
2022-12-01 23:15:25 -0500

    problema resuelto bd

--- verificar.php
+++ verificar.php
@@ -14,16 +14,18 @@
         $db = new Database();
         $connection = $db->connect();
         
-        $query = $connection->prepare("SELECT * FROM cliente");
+        $query = $connection->prepare("SELECT * FROM cliente WHERE dni=".$_POST["documento"]);
         $query->execute();
 
         $consulta = $query->fetchAll(PDO::FETCH_ASSOC);
 
         foreach($consulta as $key => $dato){
-            if ($dato["dni"]==$_POST["documento"] and $dato["contrasena"]==$_POST["contrasena"]) {
-                session_start();
-                $_SESSION["usuario"] = $dato["nombre"]." ".$dato["apellido"];
-                header("location:pagina_cliente.php");
+            if ($_POST["documento"]==$dato["dni"]) {
+                if ($_POST["contrasena"]==$dato["contrasena"]) {
+                    session_start();
+                    $_SESSION["usuario"] = $dato["nombre"]." ".$dato["apellido"];
+                    header("location:pagina_cliente.php");
+                }
             } else {
                 header("location:inicio_sesion.php");
             }
